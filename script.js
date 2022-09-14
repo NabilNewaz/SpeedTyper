0f7b4a2f39c633bb82ffae609766f991d3c1f085
NabilNewaz <nabilnewaz@gmail.com>
2022-09-14 23:59:45 +0600

    countdown close after complete fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.floor((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -113,18 +113,22 @@ const start = () => {
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      function countdownOverlayHide() {
+        countdownOverlay.style.display = "none";
+      }
+      setTimeout(countdownOverlayHide, 1000);
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+      console.log(startTime);
     }
     count--;
   }, 1000);
 };
 
 // START Countdown
-document.addEventListener("click", start);
+startBtn.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
